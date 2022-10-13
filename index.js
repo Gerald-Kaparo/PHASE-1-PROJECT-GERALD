c0f5fbd93a22510bf121d216a676c667b3ab3d12
Gerald <kapgerado05@gmail.com>
2022-10-13 11:51:38 +0300

    phase 1 project gerald

---
+++ index.js
@@ -0,0 +1,39 @@
+// Examine doc obj //
+// console.debug(...Audio: history[]): void;
+console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.URL);
+
+console.log(document.images);
+console.log(document.doctype);
+console.log(document.all);
+console.log(document.all[9].textContent='Please text');
+
+getElementById//
+console.log(document.getElementById('header title'));
+console.log(headerTitle);
+headerTitle.textContent='plant a tree';
+headerTitle.innerText='Please contribute';
+console.log(headerTitle.innerText);
+
+// GET ELEMENTS BY CLASSNAME //
+var my_goal= document.getElementsByClassName('it');
+for (var r = 0; r < FileList.length; r++){
+    div[r].style.backgroundColor='#eee';
+}
+
+//GET ELEMENTS BY TAG NAME//
+var a=document.getElementsByTagName('a');
+console.log(a);
+console.log(a[1]);
+a[1].textContent='your price';
+a[1].style.fontFamily='Times New Roman';
+a[1].style.alignContent='centre';
+
+//Query selector//
+var header= document.querySelector('h1');
+header.style.backgroundColor='light green';
+var iput= document.querySelector('input');
+input.value='contribution';
+
